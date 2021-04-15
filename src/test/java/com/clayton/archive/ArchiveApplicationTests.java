c2f399522558ccc48299224960f9d41036a8fb64
Clayton Ward <clayton@flaresoftware.com>
2021-04-14 18:38:15 -0600

    Added unit tests

--- src/test/java/com/clayton/archive/ArchiveApplicationTests.java
+++ src/test/java/com/clayton/archive/ArchiveApplicationTests.java
@@ -1,13 +1,28 @@
 package com.clayton.archive;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.clayton.archive.controllers.LoginController;
+import com.clayton.archive.controllers.ProjectsController;
+import com.clayton.archive.controllers.UsersController;
 
 @SpringBootTest
 class ArchiveApplicationTests {
-
+	@Autowired
+	private ProjectsController projectsController;
+	@Autowired
+	private LoginController loginController;
+	@Autowired
+	private UsersController usersController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(projectsController).isNotNull();
+		assertThat(loginController).isNotNull();
+		assertThat(usersController).isNotNull();
 	}
-
 }
---
+++ src/test/java/com/clayton/archive/HttpRequestTest.java
@@ -0,0 +1,23 @@
+package com.clayton.archive;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HttpRequestTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnOk() throws Exception {
+		this.mockMvc.perform(get("/api/projects")).andDo(print()).andExpect(status().isOk());
+	}
+}
